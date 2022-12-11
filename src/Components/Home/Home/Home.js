3400280114f66977c24395035a58c165da9f2448
alimransahin <mdalimransahin@gmail.com>
2022-12-11 20:57:44 +0600

    homapage structure

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Contact from '../Contact/Contact';
+import Projects from '../Projects/Projects';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Projects></Projects>
             <Contact></Contact>
         </div>
     );
---
+++ src/Components/Home/Projects/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <div className='min-h-screen max-w-screen-xl mx-auto' id='projects'>
+            <h3 className='text-3xl text-center bg-base-200 py-4'>Projects</h3>
+        </div>
+    );
+};
+
+export default Projects;
